6e6aa505eef208d8690419a4d63b03470e37f46f
augin <augin@example.com>
2026-06-29 00:50:07 +0300

    DNS: use domain field for SNI hostname in Keenetic DoT upstreams
    - Keenetic RCI ignores 'sni' and 'server-name' fields
    - The 'domain' field in tls-upstream is used as SNI hostname
    - 9.9.9.9 -> domain=dns.quad9.net
    - 1.1.1.1 -> domain=cloudflare-dns.com
    - Yandex entries unchanged (domain=ru/su/xn--p1ai)

--- router_dns.go
+++ router_dns.go
@@ -10,23 +10,19 @@ func keeneticSetupSecureDns(httpClient *http.Client, baseURL string) error {
 	dnsServers := []struct {
 		address string
 		sni     string
-		domain  string
 	}{
-		{"9.9.9.9", "dns.quad9.net", ""},
-		{"1.1.1.1", "cloudflare-dns.com", ""},
-		{"common.dot.dns.yandex.net", "", "ru"},
-		{"common.dot.dns.yandex.net", "", "su"},
-		{"common.dot.dns.yandex.net", "", "xn--p1ai"},
+		{"9.9.9.9", "dns.quad9.net"},
+		{"1.1.1.1", "cloudflare-dns.com"},
+		{"common.dot.dns.yandex.net", "ru"},
+		{"common.dot.dns.yandex.net", "su"},
+		{"common.dot.dns.yandex.net", "xn--p1ai"},
 	}
 
 	var upstreams []any
 	for _, srv := range dnsServers {
 		upstream := map[string]any{"address": srv.address}
 		if srv.sni != "" {
-			upstream["server-name"] = srv.sni
-		}
-		if srv.domain != "" {
-			upstream["domain"] = srv.domain
+			upstream["domain"] = srv.sni
 		}
 		upstreams = append(upstreams, upstream)
 	}
