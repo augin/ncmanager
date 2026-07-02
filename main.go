10f904802d16084fd5ed226520c1ce5769c37685
augin <augin@example.com>
2026-07-03 02:42:08 +0300

    v1.4.2: peer search width and font fix

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ import (
 	"golang.org/x/crypto/acme/autocert"
 )
 
-const appVersion = "1.4.1"
+const appVersion = "1.4.2"
 const dataFile = "data/config.json"
 const peersFile = "data/peers.json"
 const wgConfigFile = "/etc/wireguard/wg0.conf"
