459bbe1637309ec8fc2fdd80532eae468a9dd1d6
augin <augin@example.com>
2026-07-03 01:57:35 +0300

    v1.4.0: redesign header by awg-manager style

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ import (
 	"golang.org/x/crypto/acme/autocert"
 )
 
-const appVersion = "1.3.8"
+const appVersion = "1.4.0"
 const dataFile = "data/config.json"
 const peersFile = "data/peers.json"
 const wgConfigFile = "/etc/wireguard/wg0.conf"
