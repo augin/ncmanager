c81592ba06d1a161806981f8ab6174509e59eb17
augin <augin@example.com>
2026-07-03 14:25:23 +0300

    v1.7.0: increase card width to 1366px

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ import (
 	"golang.org/x/crypto/acme/autocert"
 )
 
-const appVersion = "1.6.0"
+const appVersion = "1.7.0"
 const dataFile = "data/config.json"
 const peersFile = "data/peers.json"
 const wgConfigFile = "/etc/wireguard/wg0.conf"
