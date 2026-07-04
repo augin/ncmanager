15031e595d2ce0ba658b53335f00a004473dff9b
augin <augin@example.com>
2026-07-05 02:35:37 +0300

    chore: bump version to 1.11.6

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ import (
 	"golang.org/x/crypto/acme/autocert"
 )
 
-const appVersion = "1.11.5"
+const appVersion = "1.11.6"
 const dataFile = "data/config.json"
 const peersFile = "data/peers.json"
 const dnsRoutesFile = "data/dns-routes.json"
