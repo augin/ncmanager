fccae115d6b7016f8623bf1d98c9154382c2b5d6
augin <augin@example.com>
2026-07-04 15:51:24 +0300

    Release v1.10.2: remove refresh button from AWG interfaces

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ import (
 	"golang.org/x/crypto/acme/autocert"
 )
 
-const appVersion = "1.10.1"
+const appVersion = "1.10.2"
 const dataFile = "data/config.json"
 const peersFile = "data/peers.json"
 const dnsRoutesFile = "data/dns-routes.json"
