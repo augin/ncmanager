00bf69f794be05c0507cd3b2597ab20132dc5df0
augin <augin@example.com>
2026-07-05 00:33:45 +0300

    release: v1.11.1 — rename settings header, restructure server settings

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ import (
 	"golang.org/x/crypto/acme/autocert"
 )
 
-const appVersion = "1.11.0"
+const appVersion = "1.11.1"
 const dataFile = "data/config.json"
 const peersFile = "data/peers.json"
 const dnsRoutesFile = "data/dns-routes.json"
